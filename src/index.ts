61b9493ce3c813fbfe0dd3c0241e0fb0985ba842
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-06-06 23:06:02 +0000

    init grammy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import { db } from "./db";
-import { sql } from "drizzle-orm";
 
-const query = sql`select "hello world" as text`;
-const result = db.get<{ text: string }>(query);
-console.log(result);
+import { Bot } from "grammy";
+
+const BOT_TOKEN = process.env.BOT_TOKEN
+
+if (typeof BOT_TOKEN === "undefined") {
+  throw new Error("BOT_TOKEN is not defined.")
+}
+
+const bot = new Bot(BOT_TOKEN); // <-- put your bot token between the "" (https://t.me/BotFather)
+
+// Reply to any message with "Hi there!".
+bot.on("message", (ctx) => ctx.reply("Hi there!"));
+
+bot.start();
