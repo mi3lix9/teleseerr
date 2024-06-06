0a8dc52d94c1aaf78dd0149758feed65be041d46
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-06-06 23:34:21 +0000

    feat: add Jellyseerr api

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,33 @@
 import { Bot } from "grammy";
 
 const BOT_TOKEN = process.env.BOT_TOKEN
+const JELLYSEERR_URL = process.env.JELLYSEERR_URL
+const JELLYSEERR_KEY = process.env.JELLYSEERR_KEY
 
 if (typeof BOT_TOKEN === "undefined") {
   throw new Error("BOT_TOKEN is not defined.")
 }
 
-const bot = new Bot(BOT_TOKEN); // <-- put your bot token between the "" (https://t.me/BotFather)
+if (typeof JELLYSEERR_URL === "undefined") {
+  throw new Error("JELLYSEERR_URL is not defined.")
+}
+
+if (typeof JELLYSEERR_KEY === "undefined") {
+  throw new Error("JELLYSEERR_KEY is not defined.")
+}
+
+
+
+const bot = new Bot(BOT_TOKEN);
 
 // Reply to any message with "Hi there!".
-bot.on("message", (ctx) => ctx.reply("Hi there!"));
+// bot.on("message", (ctx) => ctx.reply("Hi there!"));
+
+bot.command("status", async (ctx) => {
+  const res = await fetch(JELLYSEERR_URL + "/status")
+  const json = await res.json();
+  await ctx.reply(`\`\`\`JSON\n${JSON.stringify(json, null, 2)}\`\`\``, { parse_mode: "Markdown" })
+
+})
 
 bot.start();
