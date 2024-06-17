32c28c2b13f70ec9fccc42c707f4091918ce50ba
m3baid <m3baid1708@gmail.com>
2024-06-17 02:21:08 +0000

    chore: Update JELLYSEERR_URL to include "/api/v1" in env.ts

--- env.ts
+++ env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
 
   BOT_TOKEN: z.string({ message: "BOT_TOKEN is not defined." }),
-  JELLYSEERR_URL: z.string({ message: "JELLYSEERR_URL is not defined." }).url().transform((url) => url.replace(url , url + "/api/v1")),
+  JELLYSEERR_URL: z.string({ message: "JELLYSEERR_URL is not defined." }).url().transform((url) =>  url + "/api/v1"),
   JELLYSEERR_KEY: z.string({ message: "JELLYSEERR_KEY is not defined." }),
 });
 
