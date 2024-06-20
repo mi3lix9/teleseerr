5599bc7c4d108ada8ad61bde1e1a2220caa736f6
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-06-20 16:09:21 +0000

    migrate from bun to node

--- env.ts
+++ env.ts
@@ -1,11 +1,13 @@
+import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
-
   BOT_TOKEN: z.string({ message: "BOT_TOKEN is not defined." }),
-  JELLYSEERR_URL: z.string({ message: "JELLYSEERR_URL is not defined." }).url().transform((url) =>  url + "/api/v1"),
+  JELLYSEERR_URL: z
+    .string({ message: "JELLYSEERR_URL is not defined." })
+    .url()
+    .transform((url) => url + "/api/v1"),
   JELLYSEERR_KEY: z.string({ message: "JELLYSEERR_KEY is not defined." }),
 });
 
 export default envSchema.parse(process.env);
-
